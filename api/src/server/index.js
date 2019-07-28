86be6dbf00d9c2ff86e50c26588aae5ccc014f3c
Jim <jimfrenette@yahoo.com>
2019-07-28 07:52:55 -0400

    organize app entrypoint

--- api/src/server/index.js
+++ api/src/server/index.js
@@ -1,24 +1,21 @@
 const Koa = require('koa');
-
 const mongoose = require('mongoose');
-const connStr =  'mongodb://mongo:27017/default';
-
 const indexRoutes = require('./routes/index');
 const artistRoutes = require('./routes/chinook/artist');
 const albumRoutes = require('./routes/chinook/album');
 const trackRoutes = require('./routes/chinook/track');
 
-const app = new Koa();
-const PORT = process.env.PORT || 1337;
-
 /**
  * Koa app */
+const app = new Koa();
+const PORT = process.env.PORT || 1337;
 const server = app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
 });
 
 /**
  * MongoDB connection */
+const connStr =  'mongodb://mongo:27017/default';
 mongoose.connect(connStr);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
